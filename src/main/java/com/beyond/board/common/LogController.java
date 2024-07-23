fe6794f71fef1efe7cb123693f64c8682cf2f477
wisdom <wisdom0405@kakao.com>
2024-07-23 18:53:29 +0900

    gitignore파일 없어서 직접추가

--- src/main/java/com/beyond/board/common/LogController.java
+++ src/main/java/com/beyond/board/common/LogController.java
@@ -18,12 +18,12 @@ public class LogController {
         // 기존의 로그 방식 : System.out.println();
         // 문제점 1. 성능이 좋지 않음 2.로그 분류 작업 불가
         System.out.println("println 로그입니다."); // 로그 항상찍힘
-        logger.debug("debug 로그입니다.");
-        logger.info("info 로그입니다.");
-        logger.error("error 로그입니다.");
+//        logger.debug("debug 로그입니다.");
+//        logger.info("info 로그입니다.");
+//        logger.error("error 로그입니다.");
 
-//        log.info("slfj4를 통한 info로그입니다.");
-//        log.error("slfj4를 통한 error 로그입니다.");
+        log.info("slfj4를 통한 info로그입니다.");
+        log.error("slfj4를 통한 error 로그입니다.");
         // 어떤 상황에만 로그가 남도록 하고싶음
         // 분류작업을 하기위한 라이브러리가 있다.
         return "ok";
